a60f2ff8af227dfa566dd4c74d46eab85fcfa35c
ngocson18 <ngocsonxx98@gmail.com>
2019-09-23 23:57:48 +0700

    update framework

---
+++ 23092019_dotNet2/23092019_dotNet2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _23092019_dotNet2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 23092019_dotNet2/23092019_dotNet2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23092019_dotNet2.MvcApplication" Language="C#" %>
